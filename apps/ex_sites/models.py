136b7d433c5c52b3cc3d557200df4d5d6acc3259
igor <shevchenkcoigor@gmail.com>
2016-11-04 11:47:10 +0200

    add verification field and analytics google field

--- apps/ex_sites/models.py
+++ apps/ex_sites/models.py
@@ -11,6 +11,9 @@ class Info(models.Model):
     phone_numbers = models.ManyToManyField('sites.PhoneNumber', verbose_name=_('Phone numbers'), blank=True)
     email = models.EmailField(verbose_name=_('Email'), max_length=200)
     shop_short_desc = models.CharField(verbose_name=_('Short description of shop'), max_length=200, blank=True)
+    google_analytics = models.TextField(verbose_name=_('Google Analytics'), blank=True)
+    yandex_verification = models.CharField(verbose_name=_('Yandex verification'), blank=True, max_length=500)
+    google_verification = models.CharField(verbose_name=_('Google verification'), blank=True, max_length=500)
 
     def __str__(self):
         return self.site.domain
