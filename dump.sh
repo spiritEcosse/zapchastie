755d4ed3d226f332419ee1e4dab53d3fd6f06d14
igor <shevchenkcoigor@gmail.com>
2016-12-20 16:37:38 +0200

    fake

--- dump.sh
+++ dump.sh
@@ -3,7 +3,7 @@
 function dumpdata() {
     echo "Here, the recent actions of the this script."
     source /home/h5782c/virtualenv/zapchastie/2.7/bin/activate
-    ./manage.py dumpdata --indent 4 --natural-primary --natural-foreign -e contenttypes -e auth.Permission -e sessions -e admin > data/fixtures/all.json 1>>$HOME_DIR.repos_push.log
+    ./manage.py dumpdata --indent 4 --natural-primary --natural-foreign -e contenttypes -e auth.Permission -e sessions -e admin > data/fixtures/all.json
     git add .
     git commit -m "autocommit datetime on `date +'%Y-%m-%d %H:%M:%S'` (dump database and images)";
     git push
