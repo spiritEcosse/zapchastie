f8f05bf3b042f28dea64bbe4c3d27cfa9791ed7e
igor <shevchenkcoigor@gmail.com>
2016-12-20 15:39:00 +0200

    add dump.sh

---
+++ dump.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source /home/h5782c/virtualenv/zapchastie/2.7/bin/activate
+./manage.py dumpdata --indent 4 --natural-primary --natural-foreign -e contenttypes -e auth.Permission -e sessions -e admin > data/fixtures/all.json
+git add .
+git commit -m 'autocommit date +"%r %a %d %h %y" (dump data database and images)'
+git push
