ed17f387e13f1bf6a05b340f16933b34659d8639
igor <shevchenkcoigor@gmail.com>
2016-12-20 17:00:07 +0200

    fake

--- dump.sh
+++ dump.sh
@@ -5,8 +5,8 @@ function dumpdata() {
     source /home/h5782c/virtualenv/zapchastie/2.7/bin/activate
     ./manage.py dumpdata --indent 4 --natural-primary --natural-foreign -e contenttypes -e auth.Permission -e sessions -e admin > data/fixtures/all.json
     git add .
-    git commit -m "autocommit datetime on `date +'%Y-%m-%d %H:%M:%S'` (dump database and images)";
+    git commit -m "autocommit datetime on `date +'%Y-%m-%d %H:%M:%S'` (dump database and images)"
     git push
 }
 
-dumpdata | tee error.log
+dumpdata 2>&1 | tee error.log
